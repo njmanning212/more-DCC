80f7a55df872763e5a8d33b8ddf0ad4dc95530c0
Nicholas Manning <njmanning212@gmail.com>
2023-06-22 18:12:36 -0500

    solve challenge 3

--- challenges.js
+++ challenges.js
@@ -114,7 +114,9 @@ diagonalDifference([[11,2,4], [4,5,6], [10,8,-12]]) // => 15
 // Your solution for 03- here:
 
 function diagonalDifference(arr) {
-
+  let sum1 = arr[0][0] + arr[1][1] + arr[2][2]
+  let sum2 = arr[0][2] + arr[1][1] + arr[2][0]
+  return Math.abs(sum1 - sum2)
 }
 
 
